3f65724bb99baef48c8e48fa314d12b5ab064359
Emanuel Vitor Rodrigues de Souza <emanuel.v.souza@hotmail.com>
2021-08-26 13:56:53 -0300

    intro att

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,9 @@ import dynamic from "next/dynamic";
 
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
+
 import { Intro } from "../components/Intro";
+import { Portfolio } from "../components/Portfolio";
 
 export default function Home() {
   return (
@@ -27,7 +29,26 @@ export default function Home() {
         }}
       >
         <Sidebar />
-        <Intro />
+        <Flex
+          overflow="hidden"
+          w="100vw"
+          h="80vh"
+          my="6"
+          mx="auto"
+          px="6"
+          position="relative"
+          css={{
+            scrollBehavior: "smooth",
+            scrollSnapType: "y mandatory",
+            scrollbarWidth: "none",
+            scrollSnapAlign: "start",
+            "::-webkit-scrollbar": {
+              display: "none",
+            },
+          }}
+        >
+          <Intro />
+        </Flex>
       </Flex>
     </Flex>
   );
